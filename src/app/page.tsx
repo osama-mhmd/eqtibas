1c51927d04f08dcdc5f0bb05f9fd1ac7b9424a09
os-mhmd <docosmo010@gmail.com>
2024-07-22 14:51:42 +0300

    feat/quote-state-and-inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [quote, setQuote] = useState("");
+  const [owner, SetOwner] = useState(""); // Owner of quote
+
   return (
     <main>
       <section className="mt-10">
-        <div className="container">
-          <h1 className="text-center text-4xl">اقتباس</h1>
+        <div className="container mx-auto">
+          <h1 className="text-center text-4xl mb-6">اقتباس</h1>
+          <div className="grid justify-center grid-cols-[repeat(1,minmax(300px,600px))] gap-4">
+            <textarea
+              onKeyUp={(e) => setQuote((e.target as HTMLTextAreaElement).value)}
+              placeholder="الاقتباس..."
+            />
+            <input type="text" placeholder="صاحب المقولة..." />
+          </div>
+          <div className="flex justify-center items-center m-12">{quote}</div>
         </div>
       </section>
     </main>
