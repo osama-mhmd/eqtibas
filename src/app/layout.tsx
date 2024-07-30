454b626e5175543d35167b68346cdc0ba2e796b8
os-mhmd <docosmo010@gmail.com>
2024-07-30 12:02:12 +0300

    chore/style-fixes-and-restructuring-files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans_Arabic } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import ReduxProvider from "@/redux/redux-provider";
 
 const ibm = IBM_Plex_Sans_Arabic({ subsets: ["arabic"], weight: ["400"] });
