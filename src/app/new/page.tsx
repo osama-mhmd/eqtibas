91daac4f8a564289ae2e4a5129a33ac93deb7953
osama-mhmd <docosmo010@gmail.com>
2024-08-30 19:01:34 +0200

    feat/change-file-extension

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,28 +1,47 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import { toPng } from "html-to-image";
+import * as htmlToImage from "html-to-image";
 import Gallery from "@/components/gallery";
 import { useDispatch } from "react-redux";
 import { writeQuote } from "@/redux/slices/canvas-slice";
 import { useRef, useState } from "react";
 import Edit from "@/components/edit";
 import NewCanvas from "@/components/new-canvas";
-import Image from "next/image";
-import { Pencil } from "lucide-react";
+
+import { Bolt, Pencil } from "lucide-react";
+
+type AvailableExtensions = "png" | "svg";
+
+import {
+  Select,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+} from "@/components/ui/select";
 
 export default function New() {
   const canvasRef = useRef(null);
   const dispatch = useDispatch();
   const [isEditing, isEditingOrNot] = useState(false);
+  const [fileExtension, setFileExtension] =
+    useState<AvailableExtensions>("png");
 
   const downloadImage = async () => {
     if (!canvasRef.current) return;
 
-    toPng(canvasRef.current, { style: { borderRadius: "0" } })
+    const extensionCaped =
+      fileExtension.charAt(0).toUpperCase() + fileExtension.slice(1);
+
+    const functionName = "to" + extensionCaped;
+
+    htmlToImage[functionName as "toPng" | "toSvg"](canvasRef.current, {
+      style: { borderRadius: "0" },
+    })
       .then(function (dataUrl) {
         var link = document.createElement("a");
-        link.download = "eqtibas.png";
+        link.download = "eqtibas." + fileExtension;
         link.href = dataUrl;
         link.click();
       })
@@ -56,6 +75,33 @@ export default function New() {
             </div>
 
             {isEditing && <Edit closePanel={() => isEditingOrNot(false)} />}
+
+            <div className="w-[370px] sm:w-[440px] mx-auto">
+              <div className="py-2 mb-4 flex justify-between">
+                <h3 className="text-lg">إعدادات التحميل</h3>
+                <Bolt />
+              </div>
+              <div className="flex gap-1 items-center">
+                <label>صيغة التحميل:</label>
+                <Select
+                  onValueChange={(extension) =>
+                    setFileExtension(extension as AvailableExtensions)
+                  }
+                  dir="rtl"
+                  defaultValue={fileExtension}
+                >
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="اختر صيغة التحميل" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-white">
+                    <SelectGroup>
+                      <SelectItem value="svg">SVG</SelectItem>
+                      <SelectItem value="png">PNG</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
 
             <button onClick={downloadImage}>تحميل</button>
           </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+      },
     },
   },
   plugins: [],
